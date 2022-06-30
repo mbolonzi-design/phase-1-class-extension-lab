de392d6c4f1bea920e8d31badff340300284ae37
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-30 11:39:50 +0300

    commit tree 012e434244d67f14e4e862a0a9525a860390b6e0

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon {
+    constructor(arrayOfSideValues) {
+        this.sides = arrayOfSideValues
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce((a, b) => a + b)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.sides[0] + this.sides[1] <= this.sides[2] || this.sides[0] + this.sides[2] <= this.sides[1] || this.sides[1] + this.sides[2] <= this.sides[0]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.sides[0] === this.sides[1] && this.sides[0] === this.sides[2] && this.sides[0] === this.sides[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        return Math.pow(this.sides[0], 2)
+    }
+}
